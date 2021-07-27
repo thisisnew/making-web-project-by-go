ab7829f084dfb4b0fdaf3cb4f5439c0a9aecb946
thisisnew <thisisnew@naver.com>
2021-07-28 00:00:23 +0900

    Go로 만드는 웹 - Web Decorator Handler

---
+++ web10/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"making-web-project-by-go/web10/decoHandler"
+	"making-web-project-by-go/web10/myapp"
+	"net/http"
+	"time"
+)
+
+func logger(w http.ResponseWriter, r *http.Request, h http.Handler) {
+	start := time.Now()
+	log.Println("[LOGGER1] Started")
+	h.ServeHTTP(w, r)
+	log.Println("[LOGGER1] Completed time:", time.Since(start).Milliseconds())
+}
+
+func logger2(w http.ResponseWriter, r *http.Request, h http.Handler) {
+	start := time.Now()
+	log.Println("[LOGGER2] Started")
+	h.ServeHTTP(w, r)
+	log.Println("[LOGGER2] Completed time:", time.Since(start).Milliseconds())
+}
+
+func NewHandler() http.Handler {
+	h := myapp.NewHandler()
+	h = decoHandler.NewDecoHandler(h, logger)
+	h = decoHandler.NewDecoHandler(h, logger2)
+	return h
+}
+
+func main() {
+	mux := NewHandler()
+
+	http.ListenAndServe(":3000", mux)
+}
